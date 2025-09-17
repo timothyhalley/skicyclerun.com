5fb77ef1d73b7e39ea39bace07d3fdb539c2bbf2
timothyhalley <timothy.halley@gmail.com>
2025-09-16 17:49:45 -0700

    WIP: consolidate to main

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,7 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      crypto: 'crypto-browserify',
-      stream: 'stream-browserify',
-      path: 'path-browserify',
-      util: 'util/',
-      assert: 'assert/',
-      fs: 'fs-extra',
-    },
-    dedupe: ['react', 'react-dom'],
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom'],
-  },
-  plugins: [tsconfigPaths()],
+  resolve: { dedupe: ['react', 'react-dom'] },
+  optimizeDeps: { include: ['react', 'react-dom'] },
+  plugins: [tsconfigPaths()]
 });
