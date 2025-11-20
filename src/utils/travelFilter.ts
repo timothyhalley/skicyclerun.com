372cc089779620bfa9aacde2cc0f8ea423c7ce67
timothyhalley <timothy.halley@gmail.com>
2025-11-20 06:49:10 -0800

    2025-11-16 V04.016.1 🦇

--- src/utils/travelFilter.ts
+++ src/utils/travelFilter.ts
@@ -1,9 +1,10 @@
-import type { CollectionEntry } from 'astro:content';
+import type { CollectionEntry } from "astro:content";
 
-const travelFilter = ({ data }: CollectionEntry<'blog'>) => {
-  if (data.type === 'TRAVEL' || data.type === 'VLOG') return true;
+const travelFilter = ({ data }: CollectionEntry<"blog">) => {
+  if (data.type === "TRAVEL" || data.type === "VLOG") return true;
 
-  const hasCoordinates = typeof data.lat === 'number' && typeof data.lon === 'number';
+  const hasCoordinates =
+    typeof data.lat === "number" && typeof data.lon === "number";
   return hasCoordinates;
 };
 
