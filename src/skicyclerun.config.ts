91d9910de9eafaf399d2910619ee075c818e8131
Timothy Halley <your_email@example.com>
2026-04-24 06:33:44 -0700

    2026-04-24 V07.000 🛑

--- src/skicyclerun.config.ts
+++ src/skicyclerun.config.ts
@@ -13,7 +13,7 @@ export const SkiCycleRunConfig = {
   postsPerPage: 5,
   blogType: BLOG_TYPES[3], // "GENERAL"
   scheduledPostMargin: 15 * 60 * 1000,
-  version: "2026-04-23 V07.006",
+  version: "2026-04-24 V07.000",
   maxWidth: 1024,
   // maximum photos to request/display in MDXLayout galleries (4x4 => 16)
   galleryMaxPhotos: 16,
